b5eee77b19c2d2ec1640fd9c1b77820ec8c03844
Doyinsolami Olaoye <64823598+doyinsolamiolaoye@users.noreply.github.com>
2020-08-26 04:33:11 +0100

    first commit

---
+++ pair_Sum/solution.py
@@ -0,0 +1,27 @@
+def sumPair(arr,target):
+    l =0
+    r =len(arr) - 1
+    pairs = set()
+    arr.sort()
+
+    while (l < r):
+        sum = given[l] + given[r]
+
+        if (sum > target):
+            r -= 1
+        elif (sum < target):
+            l += 1
+        elif (sum  == target):
+            pairs.add((given[l],given[r]))
+            l += 1
+
+    if len(pairs) != 0:
+        return pairs
+    else:
+        return "No Pair Found"
+
+if __name__ == '__main__': 
+    target = 145
+    given = [3,6,7,4,8,10,12,15]
+    result = sumPair(given, target)
+    print(result)
